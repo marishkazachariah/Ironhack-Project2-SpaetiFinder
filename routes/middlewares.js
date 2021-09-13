51820eab253a5ae230dae3f6ae8474370bc3c1d6
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-09-13 12:28:28 +0200

    connect models to app.js + passport session configuration

---
+++ routes/middlewares.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const loginCheck = () => {
+    return (req, res, next) => {
+      // in node-basic-auth: req.session.user
+      if (req.isAuthenticated()) {
+        // proceed as planned
+        next();
+      } else {
+        res.redirect('/login');
+      }
+    }
+  }
+
+  module.exports = {
+      loginCheck: loginCheck
+  };
