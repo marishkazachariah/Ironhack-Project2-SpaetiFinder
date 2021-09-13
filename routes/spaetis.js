e58e2d4d50719afcca7f5d76b6dcefb3917edb3e
Stephen Gallagher <sgallagher92@gmail.com>
2021-09-13 18:11:14 +0200

    connected new.js page to the routes

--- routes/spaetis.js
+++ routes/spaetis.js
@@ -1,5 +1,38 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const Spaeti = require('../models/Spaeti.js');
+const User = require('../models/User.js');
+const Item = require('../models/Item.js');
+// const { uploader, cloudinary } = require('../config/cloudinary');
 
+router.get('/new', (req, res, next) => {
+  Spaeti.find().then((spaetiFromDB) => {
+    res.render('new', { spaetis: spaetiFromDB });
+  });
+});
 
+router.post('/', (req, res, next) => {
+  const { name, imageUrl, hasSeating, hasAtm, hasWC, inventory, price } =
+    req.body;
+  // const imgPath = req.file.path;
+  // const imgName = req.file.originalname;
+  // const publicId = req.file.filename;
+  Spaeti.create({
+    name: name,
+    // imgPath: imgPath,
+    // imgName: imgName,
+    hasSeating: hasSeating,
+    hasAtm: hasAtm,
+    hasWC: hasWC,
+    inventory: inventory,
+    price: price,
+  })
+
+    .then((createdSpati) => {
+      res.redirect(`/spaetis/${createdSpati._id}`);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
