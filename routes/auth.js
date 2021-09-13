fb027a7c0569b210bc50b78bacd3074e1b6904e5
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-09-13 14:11:05 +0200

    bug fix to running npm dev

--- routes/auth.js
+++ routes/auth.js
@@ -2,3 +2,5 @@ const router = require("express").Router();
 const User = require('../models/User.model');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
+
+module.exports = router;
