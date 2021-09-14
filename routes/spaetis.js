9c3d41651e5a28e92e04287516d18992ad3651ca
Rahaf <r.abualhassan@gmail.com>
2021-09-14 11:09:45 +0200

    deleted redundent code

--- routes/spaetis.js
+++ routes/spaetis.js
@@ -10,31 +10,6 @@ router.get('/new', (req, res, next) => {
   });
 });
 
-// router.post('/', (req, res, next) => {
-//   const { name, imageUrl, hasSeating, hasAtm, hasWC, inventory, price } =
-//     req.body;
-//   // const imgPath = req.file.path;
-//   // const imgName = req.file.originalname;
-//   // const publicId = req.file.filename;
-//   Spaeti.create({
-//     name: name,
-//     // imgPath: imgPath,
-//     // imgName: imgName,
-//     hasSeating: hasSeating,
-//     hasAtm: hasAtm,
-//     hasWC: hasWC,
-//     inventory: inventory,
-//     price: price,
-//   })
-
-//     .then((createdSpati) => {
-//       res.redirect(`/spaetis/${createdSpati._id}`);
-//     })
-//     .catch((err) => {
-//       next(err);
-//     });
-// });
-
 
 // Späti view
  
