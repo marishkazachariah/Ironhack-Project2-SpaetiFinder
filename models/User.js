d998a44606cf22f396829caec422c54d4ff4359e
Rahaf <r.abualhassan@gmail.com>
2021-09-16 16:32:56 +0200

    delete only by owner bug fixed

--- models/User.js
+++ models/User.js
@@ -7,11 +7,6 @@ const userSchema = new Schema({
   },
   password: String,
   avatar: String,
-  role: {
-    type: String,
-    enum: ['user', 'owner'],
-    default: 'user'
-  },
   reviews: [
     {
       type: Schema.Types.ObjectId,
